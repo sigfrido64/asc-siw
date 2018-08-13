b8b53361f9c6300c94f1a04e9f7552adada17885
Pilone Ing. Sigfrido <pilone@scuolacamerana.it>
2018-08-13 10:08:27 +0200

    Inserisce collaboratore. Controlla se il collaboratore è già presente o meno. Devo poi effettuare le necessarie operazioni in funzione di quello che ottengo.

--- collaboratori/ajax.py
+++ collaboratori/ajax.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Collaboratore
 from anagrafe.models import Persona
 from siw.decorators import ajax_has_permission_decorator
@@ -26,27 +27,12 @@ def ajax_load_tutte_persone(request):
 def ajax_check_persona_for_possible_collaborator(request):
     pk_persona = request.GET.get('pk_persona', 0)
 
-    if collaboratore_gia_presente(pk_persona):
-        return render(request, 'collaboratori/includes/corsi_list_options.html', {'corsi': lista_corsi(anno)})
+    try:
+        collaboratore = Collaboratore.objects.get(persona__pk=pk_persona)
+    except ObjectDoesNotExist:
+        return HttpResponse("Non c'è ancora")
 
-    return HttpResponse("Testa di quiz! ")
-    """
-    if collaboratore_gia_presente(pk_persona):
-        
-    if len(name_starts_with) > 2:
-        limite = 200
-    else:
-        limite = 100
-    persone = Persona.objects.all()
-    persone = persone.values('cognome', 'nome', 'data_nascita', 'pk')
-    persone = persone.filter(cognome__startswith=name_starts_with)
-    persone = persone.order_by('cognome', 'nome')[:limite]
-    # Per convertire in Json devo prima convertire in lista.
-    persone_list = list(persone)
-    return JsonResponse(persone_list, safe=False)
-    """
+    return HttpResponse("Questo c'è già ")
 
-
-def collaboratore_gia_presente(pk_persona):
-    return False
-    # collaboratore = Collaboratore.objects.get(persona=pk_persona)
+    # return render(request, 'collaboratori/includes/corsi_list_options.html', {'corsi': lista_corsi(anno)})
+    
