77835855c101f6897c70287dbab1bd3bb7662a92
Pilone Ing. Sigfrido <pilone@scuolacamerana.it>
2018-08-19 22:16:39 +0200

    Inserisce Collaboratore. Ajax load tutte persone. Creati i primi test per verificare che utente non loggato o autorizzato riceva come risposta denied.

--- collaboratori/tests/test_ajax_load_tutte_persone.py
+++ collaboratori/tests/test_ajax_load_tutte_persone.py
@@ -4,14 +4,16 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse, resolve
 from unittest import skip
+from siw.sig_http_status import HTTP_403_FORBIDDEN, HTTP_200_OK
 from accounts.models import SiwPermessi
+from ..ajax import ajax_load_tutte_persone
 
 
 # Url della vista scritto sia in modo diretto che in modo interno.
-URL = f"/collaboratori/anagrafica/inserisce-nuovo/"
-REVERSE_URL = 'collaboratori:inserisce_nuovo'
+URL = f"/collaboratori/ajax/load-persone/"
+REVERSE_URL = 'collaboratori:ajax_load_tutte_persone'
 
-@skip
+
 class GeneralTests(TestCase):
     def test_url_and_reverseurl_equality(self):
         url = reverse(REVERSE_URL)
@@ -19,23 +21,35 @@ class GeneralTests(TestCase):
 
     def test_inserisce_collaboratore_url_resolves_inserisce_collaboratore_view(self):
         view = resolve(URL)
-        self.assertEquals(view.func, propone_inserimento_collaboratore_view)
+        self.assertEquals(view.func, ajax_load_tutte_persone)
 
 
 class MyAccountTestCase(TestCase):
     """
-    Qui metto le informazioni di base per i test successivi
+    Qui metto le informazioni di base per i test successivi.
+    Metto 'username' e 'passoword' e l'url della pagina che voglio testare come reverse
     """
     def setUp(self):
         # Fake user
-        self.username = 'john'
-        self.password = 'secret123'
-        self.user = User.objects.create_user(username=self.username, email='john@doe.com', password=self.password)
-        # Dati dell'utente
-        self.myuser = User.objects.get(username=self.username)
-        # Link alla vista
-        self.url = "%s?corso=AMUC31" % reverse('attesta:ajax_load_allievi')
+        self.fake_user_username = 'john'
+        self.fake_user_password = 'secret123'
+        self.user = User.objects.create_user(username=self.fake_user_username, email='john@doe.com',
+                                             password=self.fake_user_password)
+        # Recupero tutti i Dati dell'utente, serve dopo per aggiungere i permessi.
+        self.myuser = User.objects.get(username=self.fake_user_username)
 
+
+class LoginRequiredTests(MyAccountTestCase):
+    def test_forbidden_for_not_logged_in_user(self):
+        self.response = self.client.get(URL)
+        self.assertEquals(self.response.status_code, HTTP_403_FORBIDDEN)
+
+
+class PermissionRequiredTests(MyAccountTestCase):
+    def test_deny_for_logged_in_user_not_authorized_on_app(self):
+        self.client.login(username=self.fake_user_username, password=self.fake_user_password)
+        self.response = self.client.get(URL)
+        self.assertEquals(self.response.status_code, HTTP_403_FORBIDDEN)
 
 @skip
 class NoPermsForbiddenTests(MyAccountTestCase):
