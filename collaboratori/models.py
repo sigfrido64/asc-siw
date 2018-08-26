e7cde1bde1738c171539e898aa7cdb55d20a08f2
Pilone Ing. Sigfrido <pilone@scuolacamerana.it>
2018-08-26 12:28:28 +0200

    Aggiorna Collaboratore. Sistemata la parte di aggiornamento devo vedere il controllo jqcheckbox.

--- collaboratori/models.py
+++ collaboratori/models.py
@@ -38,7 +38,7 @@ class Collaboratore(models.Model):
         Campi per la gestione di sistema.
     """
     # Il record è ancora in uso ?
-    in_uso = models.BooleanField(db_index=True, default=True, verbose_name="Il record è tutt'ora in uso ?")
+    in_uso = models.BooleanField(db_index=True, default=True, help_text="Il collaboratore è tutt'ora attivo ?")
     # Data di aggiornamento del record.
     data_aggiornamento = models.DateTimeField(auto_now=True)
     # Data di creazione del record.
