7d158c9a0f01238c680f2256639c1ae35dc71096
Pilone Ing. Sigfrido <pilone@scuolacamerana.it>
2018-08-26 22:48:02 +0200

    Sistemata la pagina dei dettagli del collaboratore in modo che stampi in modo ordinato.

--- accounts/templatetags/si_template_tags.py
+++ accounts/templatetags/si_template_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.html import mark_safe
 
 # from rolepermissions.checkers import has_permission
 from ..models import has_permission
@@ -10,3 +11,12 @@ register = template.Library()
 @register.filter(name='can')
 def can_template_tag(user, permission):
     return has_permission(user, permission)
+
+
+@register.simple_tag
+def descrizione_e_valore(descrizione, valore):
+    if descrizione:
+        risultato = descrizione + ' : ' + valore
+    else:
+        risultato = ''
+    return mark_safe(risultato)
