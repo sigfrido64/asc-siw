be206c36a48f8c284407192325450dc57353f21e
Pilone Ing. Sigfrido <sigfrido.pilone@gmail.com>
2018-08-27 13:57:55 +0200

    Modificato allineamento con data base Assocam su Sql Server per le persone.

--- anagrafe/views.py
+++ anagrafe/views.py
@@ -1,16 +1,13 @@
 # coding=utf-8
 import time
 import datetime
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from .models import Persona
 from siw.sqlserverinterface import sqlserverinterface
 
 
-# Create your views here.
-def vista1(request):
-    # Compongo la lista degli anni formativi
-    query = """
+QUERY_SELECT_PERSONE = """
     SELECT 
       [Id Persona]
       ,[Cognome]
@@ -53,8 +50,67 @@ def vista1(request):
       ,[Data Elemento]
   FROM [Assocam].[dbo].[Anagrafica Persone]
     """
+
+
+def _compila_e_salva_record(persona_asc, persona=None):
+    if not persona:
+        persona = Persona()
+
+    # Sezione ID
+    persona.asc_id = persona_asc['Id Persona']
+    persona.asc_data_elemento = persona_asc['Data Elemento']
+    persona.asc_data_aggiornamento = datetime.datetime.now()
+
+    # Dati personali
+    persona.nome = persona_asc['Nome']
+    persona.cognome = persona_asc['Cognome']
+
+    # Luogo e data di nascita, Sesso
+    persona.comune_nascita = persona_asc['Comune Nascita'] or ''
+    persona.provincia_nascita = persona_asc['Provincia Nascita'] or ''
+    persona.stato_nascita = persona_asc['Stato Nascita'] or ''
+    persona.data_nascita = persona_asc['Data Nascita'] or ''
+    persona.sesso = persona_asc['Sesso']
+
+    # Domicilio.
+    persona.indirizzo_domicilio = persona_asc['Indirizzo Domicilio'] or ''
+    persona.comune_domicilio = persona_asc['Comune Domicilio'] or ''
+    persona.cap_domicilio = persona_asc['CAP Domicilio'] or ''
+    persona.provincia_domicilio = persona_asc['Provincia Domicilio'] or ''
+
+    # Residenza.
+    persona.indirizzo_residenza = persona_asc['Indirizzo Residenza'] or ''
+    persona.comune_residenza = persona_asc['Comune Residenza'] or ''
+    persona.cap_residenza = persona_asc['CAP Residenza'] or ''
+    persona.provincia_residenza = persona_asc['Provincia Residenza'] or ''
+
+    # Codice Fiscale.
+    persona.cf = persona_asc['CF'] or ''
+
+    # Recapiti telefonici.
+    persona.tel1 = persona_asc['Tel1'] or ''
+    persona.tel2 = persona_asc['Tel2'] or ''
+    persona.tel3 = persona_asc['Tel3'] or ''
+    persona.tel4 = persona_asc['Tel4'] or ''
+
+    persona.doc_tel1 = persona_asc['DTel1'] or ''
+    persona.doc_tel2 = persona_asc['DTel2'] or ''
+    persona.doc_tel3 = persona_asc['DTel3'] or ''
+    persona.doc_tel4 = persona_asc['DTel4'] or ''
+
+    # Posta elettronica.
+    persona.mail1 = persona_asc['Mail1'] or ''
+    persona.mail2 = persona_asc['Mail2'] or ''
+
+    # Salva il record.
+    persona.save()
+
+
+# Create your views here.
+def vista1(request):
+    # Vado a leggere gli elementi dal data base SQL Server.
     # query = "SELECT * from [Assocam].[dbo].[Anagrafica Persone] "
-    persone_asc = sqlserverinterface(query)
+    persone_asc = sqlserverinterface(QUERY_SELECT_PERSONE)
 
     """
     L'elemento esite già ?
@@ -65,65 +121,24 @@ def vista1(request):
         no : non faccio nulla
         
     """
-
-    count = 0
-    totali = 0
+    count = totali = aggiornati = inseriti = 0
     for persona_asc in persone_asc:
-        persona = Persona()
+        try:
+            persona = Persona.objects.get(asc_id=persona_asc['Id Persona'])
+        except ObjectDoesNotExist:
+            _compila_e_salva_record(persona_asc)
+            inseriti += 1
+        else:
+            if persona_asc['Data Elemento'] > persona.asc_data_elemento:
+                _compila_e_salva_record(persona_asc, persona)
+                aggiornati += 1
 
-        # Sezione ID
-        persona.asc_id = persona_asc['Id Persona']
-        persona.asc_data_elemento = persona_asc['Data Elemento']
-        persona.asc_data_aggiornamento = datetime.datetime.now()
-
-        # Dati personali
-        persona.nome = persona_asc['Nome']
-        persona.cognome = persona_asc['Cognome']
-
-        # Luogo e data di nascita, Sesso
-        persona.comune_nascita = persona_asc['Comune Nascita'] or ''
-        persona.provincia_nascita = persona_asc['Provincia Nascita'] or ''
-        persona.stato_nascita = persona_asc['Stato Nascita'] or ''
-        persona.data_nascita = persona_asc['Data Nascita'] or ''
-        persona.sesso = persona_asc['Sesso']
-
-        # Domicilio.
-        persona.indirizzo_domicilio = persona_asc['Indirizzo Domicilio'] or ''
-        persona.comune_domicilio = persona_asc['Comune Domicilio'] or ''
-        persona.cap_domicilio = persona_asc['CAP Domicilio'] or ''
-        persona.provincia_domicilio = persona_asc['Provincia Domicilio'] or ''
-
-        # Residenza.
-        persona.indirizzo_residenza = persona_asc['Indirizzo Residenza'] or ''
-        persona.comune_residenza = persona_asc['Comune Residenza'] or ''
-        persona.cap_residenza = persona_asc['CAP Residenza'] or ''
-        persona.provincia_residenza = persona_asc['Provincia Residenza'] or ''
-
-        # Codice Fiscale.
-        persona.cf = persona_asc['CF'] or ''
-
-        # Recapiti telefonici.
-        persona.tel1 = persona_asc['Tel1'] or ''
-        persona.tel2 = persona_asc['Tel2'] or ''
-        persona.tel3 = persona_asc['Tel3'] or ''
-        persona.tel4 = persona_asc['Tel4'] or ''
-
-        persona.doc_tel1 = persona_asc['DTel1'] or ''
-        persona.doc_tel2 = persona_asc['DTel2'] or ''
-        persona.doc_tel3 = persona_asc['DTel3'] or ''
-        persona.doc_tel4 = persona_asc['DTel4'] or ''
-
-        # Posta elettronica.
-        persona.mail1 = persona_asc['Mail1'] or ''
-        persona.mail2 = persona_asc['Mail2'] or ''
-
-        persona.save()
         count = count + 1
         totali = totali + 1
-        if count > 10:
-            print(__name__ + "Processati altri 10 alle " + datetime.datetime.now().__str__() + " per un totale di " +
+        if count > 50:
+            print(__name__ + "Processati altri 50 alle " + datetime.datetime.now().__str__() + " per un totale di " +
                   totali.__str__())
             count = 0
 
-    # Prendo il primo anno per la query che segue.
-    return HttpResponse("Fatto !")
+    # Compongo la risposta.
+    return HttpResponse(f"Fatto !, aggiornati : {aggiornati}, inseriti : {inseriti}")
