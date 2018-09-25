cb24b3ede76bbd52465daa6ff4a8b283e2bcc874
Pilone Ing. Sigfrido <sigfrido.pilone@gmail.com>
2018-09-25 21:45:50 +0200

    Test su view cdc completo. Adesso devo sistemare il file html e fare qualche refactoring.

--- amm/tests/test_cdc_view.py
+++ amm/tests/test_cdc_view.py
@@ -2,8 +2,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse, resolve
-from django.forms import ModelForm
-from unittest import skip
 from accounts.models import SiwPermessi
 from amm.views import cdc
 __author__ = "Pilone Ing. Sigfrido"
@@ -70,123 +68,7 @@ class FormGeneralTests(MyAccountTestCase):
         view = resolve(URL)
         self.assertEquals(view.func, cdc)
         
-    @skip("In questa vista non sono presenti form per cui non c'è csrfmiddlewaretoken")
-    def test_csrf(self):
-        # Il crfmiddlewaretoken ci deve essere
-        self.assertContains(self.response, 'csrfmiddlewaretoken')
-
-    @skip("In questa vista non sono presenti form per cui non c'è un form")
-    def test_contains_form(self):
-        # Devo avere un oggetto di tipo form
-        form = self.response.context['form']
-        self.assertIsInstance(form, ModelForm)
-
-    @skip
-    def test_use_template(self):
+    def test_use_correct_template(self):
         # Controllo che usi il template corretto.
-        self.assertTemplateUsed(self.response, 'attesta/mdl.html',
+        self.assertTemplateUsed(self.response, 'amm/cdc_list.html',
                                 "Non è stato usato il template corretto")
-
-    @skip
-    def test_form_inputs(self):
-        """
-        La vista deve contenere :
-        L'Anno Formativo, la data di stampa, la lista dei corsi, la lista delle stampe e la Tabella degli allievi.
-        """
-        self.assertContains(self.response, '<select id="anni_formativi"', 1)
-        self.assertContains(self.response, '<input type="date" id="data_stampa"/>', 1)
-        self.assertContains(self.response, '<select id="lista_corsi"', 1)
-        self.assertContains(self.response, '<select id="lista_stampe"', 1)
-        self.assertContains(self.response, '<tbody id="lista_allievi">', 1)
-
-
-    """
-    Testing : Accesso e visibilità della vista in ogni suo aspetto.
-    
-    ## Nei menù la vista esce solo se sono loggato e se ho i permessi
-    
-    
-    
-    # Creo l'utente base per le prove.
-    def setUp(self):
-        self.username = 'john'
-        self.password = 'secret123'
-        self.user = User.objects.create_user(username=self.username, email='john@doe.com', password=self.password)
-        self.myuser = User.objects.get(username=self.username)
-
-    def test_logged_in_uses_correct_template_and_logout(self):
-        # Provo ad accedere alla home senza loggarmi e mi deve rimandare al login con il next alla Home.
-        resp = self.client.get(reverse('home'))
-        self.assertRedirects(resp, '/login/?next=/')
-
-        # Allora loggo l'utente.
-        self.client.login(username='john', password='secret123')
-        resp = self.client.get(reverse('home'))
-
-        # Controllo che il login sia andato a buon fine.
-        self.assertEqual(str(resp.context['user']), 'john', "Non sono riuscito a loggarmi")
-        
-        # Controllo che la risposta sia un 'success'
-        self.assertEqual(resp.status_code, 200, "Non ho recuperato la pagina di login")
-
-        # Controllo se viene usato il template corretto
-        self.assertTemplateUsed(resp, 'home.html', "Non è stato usato il template corretto")
-        
-        # Controllo che nel testo ci sia la voce logout 'Esci'
-        self.assertTrue(b'Esci' in resp.content, "Manca la voce per il Logout")
-
-        # A questo punto logout.
-        self.client.logout()
-
-        # Controllo che la risposta sia un 'success'
-        self.assertEqual(resp.status_code, 200, "Logout non andato a buon fine")
-"""
-
-"""
-
-
-class SuccessfulMyAccountTests(MyAccountTestCase):
-    def setUp(self):
-        super().setUp()
-        self.client.login(username=self.username, password=self.password)
-        self.response = self.client.post(self.url, {
-            'first_name': 'John',
-            'last_name': 'Doe',
-            'email': 'johndoe@example.com',
-        })
-
-    def test_redirection(self):
-        "
-        A valid form submission should redirect the user
-        "
-        self.assertRedirects(self.response, self.url)
-
-    def test_data_changed(self):
-        ""
-        refresh the user instance from database to get the updated data.
-        ""
-        self.user.refresh_from_db()
-        self.assertEquals('John', self.user.first_name)
-        self.assertEquals('Doe', self.user.last_name)
-        self.assertEquals('johndoe@example.com', self.user.email)
-
-
-class InvalidMyAccountTests(MyAccountTestCase):
-    def setUp(self):
-        super().setUp()
-        self.client.login(username=self.username, password=self.password)
-        self.response = self.client.post(self.url, {
-            'first_name': 'longstring' * 100
-        })
-
-    def test_status_code(self):
-        ""
-        An invalid form submission should return to the same page
-        ""
-        self.assertEquals(self.response.status_code, 200)
-
-    def test_form_errors(self):
-        form = self.response.context['form']
-        self.assertTrue(form.errors)
-
-"""
