8f1d5690ca3376daf7d9ce94f1fcdbd0594d0f18
Pilone Ing. Sigfrido <sigfrido.pilone@gmail.com>
2018-12-30 01:12:57 +0100

    Finito di ripulire base.js Ora mi posso concentrare sulla pagina acquisti.

--- static/js/base.js
+++ static/js/base.js
@@ -4,39 +4,36 @@ $(document).ready(function () {
   let set_af_url = comboAnnoFormativo.attr("set-af-url");
   let af_val = comboAnnoFormativo.attr("af");
 
-          let source_af = {
-             datatype: "json",
-             datafields: [
-                 {name: 'pk'},
-                 {name: 'anno_formativo'}
-             ],
-             url: load_af_url
-          };
-          let dataAdapter_af = new $.jqx.dataAdapter(source_af);
+  let source_af = {
+     datatype: "json",
+     datafields: [
+         {name: 'pk'},
+         {name: 'anno_formativo'}
+     ],
+     url: load_af_url
+  };
+  let dataAdapter_af = new $.jqx.dataAdapter(source_af);
 
-          $("#id_anno_formativo").jqxComboBox({placeHolder: 'Anno Formativo', source: dataAdapter_af,
-            displayMember: "anno_formativo", valueMember: "pk"});
-          $("#id_anno_formativo").on('bindingComplete', function (event) {
-            $("#id_anno_formativo").val(af_val);
-            });
-          $("#id_anno_formativo").on('change', function (event)
-           {
-              var args = event.args;
-              if (args) {
-              // index represents the item's index.
-              var index = args.index;
-              var item = args.item;
-              // get item's label and value.
-              var label = item.label;
-              var value = item.value;
-              var type = args.type; // keyboard, mouse or null depending on how the item was selected.
+  comboAnnoFormativo.jqxComboBox({placeHolder: 'Anno Formativo', source: dataAdapter_af,
+    displayMember: "anno_formativo", valueMember: "pk"});
 
-              $.ajax({                       // Inizializza la richiesta AJAX
-                  url: set_af_url,                    // Imposta l'url della richiesta (= localhost:8000/attesta/ajax/load-corsi/)
-                  data: {
-                    'anno_formativo': label
-                  }
-              });
-           }
-           });
+  comboAnnoFormativo.on('bindingComplete', function (event) {
+    comboAnnoFormativo.val(af_val);
+  });
+
+  comboAnnoFormativo.on('change', function (event) {
+    let args = event.args;
+    if (args) {
+      // index represents the item's index.
+      let item = args.item;
+      // get item's label and value.
+      let label = item.label;
+      $.ajax({
+          url: set_af_url,
+          data: {
+            'anno_formativo': label
+          }
+      });
+    }
+  });
 });
