337280d0728781c2b53f6cb8e1ddf0c5a37eebee
Pilone Ing. Sigfrido <sigfrido.pilone@gmail.com>
2019-01-28 12:48:19 +0100

    Sistemato il nome della queue su cui mandare in esecuzione ed inserito nel settings di Django.

--- anagrafe/views.py
+++ anagrafe/views.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from django.http import HttpResponse
+from django.conf import settings
 from .tasks import allinea_aziende_task, allinea_persone_task, allinea_contatti_aziende_task
 from celery import group, chain
 from siw.context_processor import get_current_username
@@ -11,5 +12,5 @@ def allinea_tutto_da_sql_server_view(request):
     # allinea_aziende_task.delay()
     contatti_e_persone = group(allinea_persone_task.si(user), allinea_contatti_aziende_task.si(user))
     lavoro = chain(allinea_aziende_task.si(user), contatti_e_persone)
-    lavoro.apply_async()
+    lavoro.apply_async(queue=settings.CELERYSI_DB_QUEUE_NAME)
     return HttpResponse("Processo lanciato in background")
