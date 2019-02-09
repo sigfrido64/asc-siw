218baf509a8b10f620574659e69f35d8976165de
Pilone Ing. Sigfrido <sigfrido.pilone@gmail.com>
2019-02-09 11:27:40 +0100

    Sistemato il codice del test.

--- acquisti/tests/test_model_acquisto_su_web.py
+++ acquisti/tests/test_model_acquisto_su_web.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 from amm.models.mixins import AnnoFormativo
 from amm.models.centri_di_costo import CentroDiCosto
-from acquisti.models import AcquistoSuWeb, RipartizioneSpesaPerCDC
+from acquisti.models import AcquistoSuWeb, RipartizioneAcquistoWebPerCDC
 
 from unittest import skip
 
@@ -50,53 +50,50 @@ class ModelloRipartizioniTests(TestCase):
         self.acquisto_web.save()
 
     def carica_ripartizione_indetraibile(self):
-        self.ripartizione1 = RipartizioneSpesaPerCDC(acquisto=self.acquisto_web, cdc=self.cdc1,
-                                                     percentuale_di_competenza=50)
+        self.ripartizione1 = RipartizioneAcquistoWebPerCDC(acquisto_web=self.acquisto_web, cdc=self.cdc1,
+                                                           percentuale_di_competenza=50)
         self.ripartizione1.save()
 
     def carica_ripartizione_detraibile(self):
-        self.ripartizione2 = RipartizioneSpesaPerCDC(acquisto=self.acquisto_web, cdc=self.cdc2,
-                                                     percentuale_di_competenza=50)
+        self.ripartizione2 = RipartizioneAcquistoWebPerCDC(acquisto_web=self.acquisto_web, cdc=self.cdc2,
+                                                           percentuale_di_competenza=50)
         self.ripartizione2.save()
 
     def test_calcoli_iva_detraibile_e_non_su_spesa(self):
         self.assertEqual(self.acquisto_web.iva_comunque_indetraibile, 44)
         self.assertEqual(self.acquisto_web.iva_potenzialmente_detraibile, 176)
 
-    @skip("Slata")
     def test_calcolo_corretto_del_costo_di_ripartizione_indetraibile(self):
         self.carica_ripartizione_indetraibile()
         self.assertEqual(self.ripartizione1.costo_totale, 610)
 
-    @skip("Slata")
     def test_massima_percentuale_della_singala_ripartizione(self):
         # Poi provo con cdc con IVA indetraibile ma metto percentuale maggiore del 100%.
-        ripartizione2 = RipartizioneSpesaPerCDC(acquisto=self.spesa, cdc=self.cdc2, percentuale_di_competenza=160)
+        ripartizione2 = RipartizioneAcquistoWebPerCDC(acquisto_web=self.acquisto_web, cdc=self.cdc2,
+                                                      percentuale_di_competenza=160)
         with self.assertRaises(ValidationError):
             ripartizione2.clean()
 
-    @skip("Slata")
     def test_massima_percentuale_della_somma_di_tutte_le_ripartizioni(self):
         # Poi provo con cdc con IVA indetraibile ma metto percentuale troppo alta sommata alla prima.
         self.carica_ripartizione_indetraibile()
-        ripartizione2 = RipartizioneSpesaPerCDC(acquisto=self.spesa, cdc=self.cdc2, percentuale_di_competenza=60)
+        ripartizione2 = RipartizioneAcquistoWebPerCDC(acquisto_web=self.acquisto_web, cdc=self.cdc2,
+                                                      percentuale_di_competenza=60)
         with self.assertRaises(ValidationError):
             ripartizione2.clean()
 
-    @skip("Slata")
     def test_calcolo_corretto_del_costo_di_ripartizione_detraibile(self):
         self.carica_ripartizione_detraibile()
         self.assertEqual(self.ripartizione2.costo_totale, 522)
 
-    @skip("Slata")
     def test_calcolo_del_costo_totale_della_spesa(self):
         self.carica_ripartizione_indetraibile()
         self.carica_ripartizione_detraibile()
-        self.spesa.calcola_costo_totale()
-        self.assertEqual(self.spesa.costo, 1132)
+        self.acquisto_web.calcola_costo_totale()
+        self.assertEqual(self.acquisto_web.costo, 1132)
 
-    @skip("Slata")
     def test_percentuale_di_competenza_maggiore_di_zero(self):
-        ripartizione = RipartizioneSpesaPerCDC(acquisto=self.spesa, cdc=self.cdc2, percentuale_di_competenza=0)
+        ripartizione = RipartizioneAcquistoWebPerCDC(acquisto_web=self.acquisto_web, cdc=self.cdc2,
+                                                     percentuale_di_competenza=0)
         with self.assertRaises(ValidationError):
             ripartizione.clean()
